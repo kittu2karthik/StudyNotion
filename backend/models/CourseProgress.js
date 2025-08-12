9919260deb0959b641627712676de3982cda86a9
kittu2karthik <kittu2karthik@gmail.com>
2025-08-12 23:00:58 +0530

    added Models

---
+++ backend/models/CourseProgress.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const courseProgress = new mongoose.Schema({
+  courseID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+  completedCourse: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SubSection",
+  },
+});
+
+module.exports = mongoose.model("CourseProgess", courseProgress);
